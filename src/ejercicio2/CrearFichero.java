9719e8bc7f6b862eecf386b9e98a1f7c5b68091d
15Carmen <carmen.martin@iesnervion.es>
2022-10-04 09:53:20 +0200

    commit4

--- src/ejercicio2/CrearFichero.java
+++ src/ejercicio2/CrearFichero.java
@@ -5,34 +5,19 @@ import java.io.*;
 public class CrearFichero {
     public static void main(String[] args) {
 
+        String padre="doc/", hijo="AD/";
+        recorre(padre,hijo);
+    }
+    public static void recorre(String padre, String hijo){
+        String texto;
         try {
-            String html="<html>\n" +
-                        "   <head>\n" +
-                        "      <title> [UA01] </title>\n" +
-                        "   </head>\n" +
-                        "   <body>\n" +
-                        "      <h1>[C:\\Users\\cmartin\\eclipse-workspace\\AccesoDatos1\\AD\\UA01\\ficheroHTML.txt]</h1>\n" +
-                        "      <h3>Autor: [Carmen_Martin_Nunez]</h3>\n" +
-                        "   </body>\n" +
-                        "</html>";
-            File fichero = new File("C:\\Users\\cmartin\\eclipse-workspace\\AccesoDatos1\\AD\\UA01\\ficheroHTML.txt");
+            File fPadre=new File(padre);
 
-            fichero.createNewFile();
-
-            if (fichero.exists()){
-                FileWriter fw=new FileWriter("C:\\Users\\cmartin\\eclipse-workspace\\AccesoDatos1\\AD\\UA01\\ficheroHTML.txt");
-                BufferedWriter bw=new BufferedWriter(fw);
-                bw.write(html);
-                bw.close();
-
-                System.out.println("El fichero se ha creado con éxito");
-
-            }else {
-                System.out.println("El fichero no se ha podido crear");
+            if (fPadre.isDirectory()){
+                FileWriter fw = new FileWriter(padre+"/index.html");
             }
-
-        } catch (Exception e){
-            e.getMessage();
+        }catch(Exception e){
+            System.out.println("Error!"+e.getMessage());
         }
     }
 }
